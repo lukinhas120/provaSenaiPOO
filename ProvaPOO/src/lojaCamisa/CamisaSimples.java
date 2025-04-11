65866c8432a60058e010e8c1a93361cf2c26cbd4
lukinhas120 <lucasvinicius201090@hotmail.com>
2025-04-11 20:30:44 -0300

    Alteração na classe CamisaSimples

--- ProvaPOO/src/lojaCamisa/CamisaSimples.java
+++ ProvaPOO/src/lojaCamisa/CamisaSimples.java
@@ -5,7 +5,11 @@ public class CamisaSimples extends Camisa{
 	public CamisaSimples() {
 		
 	}
-
+	
+	public CamisaSimples(String cor, String estampa, double preco, String tamanho) {
+		super(cor, tamanho, preco);
+		
+	}
 	@Override
 	public void exibirDetalhes() {
 		System.out.println("Cor " + getCor() + "\nPreço R$" + getPreco() + "\nTamanho da camisa " + getTamanho());
